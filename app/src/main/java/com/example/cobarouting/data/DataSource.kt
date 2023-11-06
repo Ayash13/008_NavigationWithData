fdeae0b7b19dafcfb35eae7e5509696329c765b3
Ayash13 <m.ayashal.f@gmail.com>
2023-11-06 13:48:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cobarouting/data/DataSource.kt
@@ -0,0 +1,13 @@
+package com.example.cobarouting.data
+
+import com.example.cobarouting.R
+
+object DataSource {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.leci
+    )
+}
